a93e1532af58ae417854d3055cec88555cb15be6
Rahul Sutradhar <rahulsd380@gmail.com>
2025-02-27 21:55:07 +0600

    Setup project

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Shared/Navbar/Navbar";
+import Footer from "../components/Shared/Footer/Footer";
+
+
+const MainLayout = () => {
+    return (
+        <div>
+            <Navbar/>
+            <Outlet/>
+            <Footer/>
+            {/* <ScrollToTop /> */}
+        </div>
+    );
+};
+
+export default MainLayout;
---
+++ src/routes/routes.tsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import NotFound from "../pages/NotFound/NotFound";
+import Home from "../pages/Home/Home";
+
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+
+    ],
+  },
+]);
